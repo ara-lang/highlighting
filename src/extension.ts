b5e46c029f1bda3142511520fec11b121ea472ab
Nicolas Hedger <nicolas@hedger.ch>
2023-01-11 16:07:25 +0100

    chore: update extension name

--- src/extension.ts
+++ src/extension.ts
@@ -4,12 +4,12 @@ import { ExtensionContext } from 'vscode';
  * This method is called by VS Code when the extension is activated.
  */
 export const activate = (context: ExtensionContext) => {
-    console.log('The ARA VS Code extension is now active.');
+    console.log('Ara highlighting extension is now active.');
 };
 
 /**
  * This method is called by VS Code when the extension is deactivated.
  */
 export const deactivate = () => {
-    console.log('The ARA VS Code extension is now inactive.');
+    console.log('Ara highlighting extension is now inactive.');
 };
