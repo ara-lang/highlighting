3aa6571f49a0d968139266fa97fb42773b6ca8e9
Nicolas Hedger <nicolas@hedger.ch>
2023-01-12 08:26:31 +0100

    chore: ignore warning

--- src/extension.ts
+++ src/extension.ts
@@ -3,6 +3,7 @@ import { ExtensionContext } from 'vscode';
 /**
  * This method is called by VS Code when the extension is activated.
  */
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const activate = (context: ExtensionContext) => {
     console.log('Ara highlighting extension is now active.');
 };
